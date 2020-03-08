86d5b44cf297d9154a209e2855508d9859ee9409
hogehoge0123 <hogehoge0123.address@gmail.com>
2020-03-08 18:08:48 +0900

    特徴分類機を用いて「目」を検出し、その位置に矩形を表示するように

--- opencv-game-test/GameViewController.mm
+++ opencv-game-test/GameViewController.mm
@@ -7,6 +7,7 @@
 //
 #ifdef __cplusplus
 #import <opencv2/opencv.hpp>
+#import <opencv2/core.hpp>
 #import <opencv2/imgproc.hpp>
 #import <opencv2/imgcodecs/ios.h>
 #import <opencv2/imgproc/types_c.h>
@@ -26,6 +27,10 @@
     
     UIImage* _videoImage;
     UIImageView* _videoImageView;
+    
+    cv::CascadeClassifier _cascade;
+    double _scaleFactor;
+    int _minNeighbors;
 }
 
 - (void)viewDidLoad
@@ -50,6 +55,15 @@
     [grayImageView setCenter:CGPointMake(grayImage.size.width/2, grayImage.size.height*1.5)];
     [_view addSubview:grayImageView];
 #endif
+    
+    // 特徴分類機を作成
+    NSBundle* bundle = [NSBundle mainBundle];
+    NSString* resourceDirectoryPath = [bundle bundlePath];
+    NSString* path = [resourceDirectoryPath stringByAppendingString: @"/haarcascade_eye.xml"];
+    std::string filename = [path UTF8String];
+    _cascade.load(filename);
+    _scaleFactor = 1.1;
+    _minNeighbors = 3;
 
     // カメラ画像転送用のビューを作る
     _videoImage = [UIImage imageNamed:@"video_frame.png"];
@@ -89,6 +103,7 @@
 {
     // Do some OpenCV stuff with the image
 #if 0
+    // グレースケール化
     cv::Mat image_copy;
     cvtColor(image, image_copy, cv::COLOR_BGR2BGRA);
     // invert image
@@ -98,6 +113,31 @@
     cvtColor(image_copy, bgr, cv::COLOR_BGR2BGRA);
     cvtColor(bgr, image, cv::COLOR_BGR2BGRA);
 #endif
+    
+    // 「目」を検出する
+    cv::Mat img;
+    cvtColor(image, img, cv::COLOR_BGR2BGRA);
+    std::vector<double> weights;
+    std::vector<int> levels;
+    std::vector<cv::Rect> detections;
+    cv::Size min;
+    cv::Size max;
+    _cascade.detectMultiScale(img,             // image
+                              detections,      // objects
+                              levels,          // reject levels
+                              weights,         // level weights
+                              _scaleFactor,    // scale factor
+                              _minNeighbors,   // min neighbors
+                              0,               // flags
+                              min,             // min size
+                              max,             // max size
+                              true);           // output reject levels
+    
+    // 「目」の位置に矩形を描画する
+    for (auto object : detections) {
+        cv::rectangle(img, object, cv::Scalar(255, 255, 255));
+    }
+    cvtColor(img, image, cv::COLOR_BGR2BGRA);
 }
 #endif
 
